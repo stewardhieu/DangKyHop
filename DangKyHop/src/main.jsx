8d516958238356d48495b5f6dd0d1b2965e25208
stewardhieu <doanvyhieu.ubtlu@gmail.com>
2026-03-19 15:12:51 +0700

    fix: wrap App in AuthProvider to resolve useAuth context error

--- DangKyHop/src/main.jsx
+++ DangKyHop/src/main.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.css';
+import { AuthProvider } from './contexts/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
